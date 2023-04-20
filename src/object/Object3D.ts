3e36355bbbbc126285e1c0dc528921b0db84f1f8
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2023-04-20 08:00:38 +0700

    feat: differensiate animatortransform

--- src/object/Object3D.ts
+++ src/object/Object3D.ts
@@ -1,26 +1,65 @@
+import { IDENTITY_MATRIX, Matrix } from "../matrix/Matrix";
 import { Transform } from "../matrix/Transform";
 import { Color } from "./Color";
 import { Component } from "./Component";
 import { Face } from "./Face";
 import { Vertex } from "./Vertices";
 
+const TRANSFORM_IDX = 0;
+
 export class Object3D {
   private childList: Object3D[] = [];
-  private transformData: Transform = new Transform();
+  private transformMapData: Map<number, Transform> = new Map();
   private componentList: Component[] = [];
+  private isTransformChange = false;
+  private transformCache = IDENTITY_MATRIX;
 
   constructor(
     private name: string,
     private facesList: Face[],
     private joinPointvert: Vertex
-  ) {}
+  ) {
+    this.transformMapData.set(TRANSFORM_IDX, new Transform());
+  }
 
   get id() {
     return this.name;
   }
 
-  get transform() {
-    return this.transformData;
+  getTransform(key: number) {
+    let result = this.transformMapData.get(key);
+
+    if (!result) {
+      result = new Transform();
+      this.transformMapData.set(key, result);
+    }
+
+    return result;
+  }
+
+  setTransform(key: number, matrix: Matrix) {
+    if (!this.transformMapData.has(key)) {
+      this.transformMapData.set(key, new Transform());
+    }
+
+    this.transformMapData.get(key).updateMatrix(matrix);
+    this.isTransformChange = true;
+  }
+
+  get matrix() {
+    if (this.isTransformChange) {
+      let total = IDENTITY_MATRIX;
+      const keys = [...this.transformMapData.keys()].sort((a, b) => a - b);
+
+      for (const transformKey of keys) {
+        const transform = this.transformMapData.get(transformKey);
+        total = Matrix.multiply(total, transform.matrix);
+      }
+
+      this.transformCache = total;
+    }
+
+    return this.transformCache;
   }
 
   get childs() {
