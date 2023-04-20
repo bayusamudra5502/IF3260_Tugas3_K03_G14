fd04b46cfb98bbaa9d88465bafcf2351c822445e
khelli07 <khelli07@users.noreply.github.com>
2023-04-20 04:02:56 +0000

    deploy: ca25d7416d225cc52e1d6149e68d0c5fd1fcf08e

--- static/ui/TransformUi.js
+++ static/ui/TransformUi.js
@@ -19,7 +19,6 @@ import { Translation } from "../transform/Translation.js";
 import { Listenable } from "../util/Listenable.js";
 var TransformUi = /** @class */ (function (_super) {
     __extends(TransformUi, _super);
-    //   private currentRotationAxis: RotationAxis;
     function TransformUi(options) {
         if (options === void 0) { options = TRANSFORM_UI_DEFAULT_OPTIONS; }
         var _this = _super.call(this) || this;
@@ -28,7 +27,6 @@ var TransformUi = /** @class */ (function (_super) {
         _this.currentTranslation = new Translation();
         _this.currentRotation = new Rotation();
         _this.currentScale = new Scaling();
-        // this.currentRotationAxis = RotationAxis.X;
         _this.bind();
         _this.update();
         return _this;
@@ -70,12 +68,6 @@ var TransformUi = /** @class */ (function (_super) {
         var translationX = document.getElementById(this.options.idTranslationX);
         var translationY = document.getElementById(this.options.idTranslationY);
         var translationZ = document.getElementById(this.options.idTranslationZ);
-        // const rotationType = document.getElementById(
-        //   this.options.idRotationType
-        // ) as HTMLSelectElement;
-        // const rotationValue = document.getElementById(
-        //   this.options.idRotationValue
-        // ) as HTMLInputElement;
         var anglex = document.getElementById(this.options.idRotationValueX);
         var angley = document.getElementById(this.options.idRotationValueY);
         var anglez = document.getElementById(this.options.idRotationValueZ);
@@ -86,14 +78,6 @@ var TransformUi = /** @class */ (function (_super) {
         translationX.value = "".concat(this.currentTranslation.X);
         translationY.value = "".concat(this.currentTranslation.Y);
         translationZ.value = "".concat(this.currentTranslation.Z);
-        // rotationType.value = `${this.currentRotation.rotationAxis}`;
-        // if (this.currentRotation.rotationAxis === RotationAxis.X) {
-        //     rotationValue.value = `${this.currentRotation.rotationAngleX}`;
-        // } else if (this.currentRotation.rotationAxis === RotationAxis.Y) {
-        //     rotationValue.value = `${this.currentRotation.rotationAngleY}`;
-        // } else if (this.currentRotation.rotationAxis === RotationAxis.Z) {
-        //     rotationValue.value = `${this.currentRotation.rotationAngleZ}`;
-        // }
         anglex.value = "".concat(this.currentRotation.rotationAngleX);
         angley.value = "".concat(this.currentRotation.rotationAngleY);
         anglez.value = "".concat(this.currentRotation.rotationAngleZ);
@@ -107,77 +91,45 @@ var TransformUi = /** @class */ (function (_super) {
         var translationX = document.getElementById(this.options.idTranslationX);
         var translationY = document.getElementById(this.options.idTranslationY);
         var translationZ = document.getElementById(this.options.idTranslationZ);
-        // const rotationType = document.getElementById(
-        //   this.options.idRotationType
-        // ) as HTMLSelectElement;
-        // const rotationValue = document.getElementById(
-        //   this.options.idRotationValue
-        // ) as HTMLInputElement;
         var anglex = document.getElementById(this.options.idRotationValueX);
         var angley = document.getElementById(this.options.idRotationValueY);
         var anglez = document.getElementById(this.options.idRotationValueZ);
         var scaleX = document.getElementById(this.options.idScaleX);
         var scaleY = document.getElementById(this.options.idScaleY);
         var scaleZ = document.getElementById(this.options.idScaleZ);
-        transformIndex.onchange = function () {
+        transformIndex.oninput = function () {
             var value = parseInt(transformIndex.value);
             !Number.isNaN(value) && (_this.currentTransformIndex = value);
             _this.update();
         };
-        translationX.onchange = function () {
+        translationX.oninput = function () {
             var value = parseFloat(translationX.value);
             !Number.isNaN(value) &&
-                // this.currentTranslation.X = value
                 _this.currentTranslation.configure({
                     x: value,
                 });
             _this.update();
+            changeInnerText("".concat(_this.options.idTranslationX, "-value"), value.toString());
         };
-        translationY.onchange = function () {
+        translationY.oninput = function () {
             var value = parseFloat(translationY.value);
             !Number.isNaN(value) &&
-                // this.currentTranslation.y = value
                 _this.currentTranslation.configure({
                     y: value,
                 });
             _this.update();
+            changeInnerText("".concat(_this.options.idTranslationY, "-value"), value.toString());
         };
-        translationZ.onchange = function () {
+        translationZ.oninput = function () {
             var value = parseFloat(translationZ.value);
             !Number.isNaN(value) &&
-                // this.currentTranslation.z = value
                 _this.currentTranslation.configure({
                     z: value,
                 });
             _this.update();
+            changeInnerText("".concat(_this.options.idTranslationZ, "-value"), value.toString());
         };
-        // rotationType.onchange = () => {
-        //   const value = rotationType.value as RotationAxis;
-        //   this.currentRotation.configure({
-        //     axis: value,
-        //   });
-        //   this.update();
-        // };
-        // rotationValue.onchange = () => {
-        //   const value = parseFloat(rotationValue.value);
-        //   !Number.isNaN(value) &&
-        //     (this.currentRotation.rotationAxis === RotationAxis.X
-        //         ? this.currentRotation.configure({
-        //             angleX: value,
-        //             })
-        //         : this.currentRotation.rotationAxis === RotationAxis.Y
-        //         ? this.currentRotation.configure({
-        //             angleY: value,
-        //             })
-        //         : 
-        //         this.currentRotation.configure({
-        //             angleZ: value,
-        //             })
-        //     );
-        //   this.update();
-        // };
-        anglex.onchange = function () {
-            // this.currentRotationAxis = RotationAxis.X;
+        anglex.oninput = function () {
             var value = parseFloat(anglex.value);
             !Number.isNaN(value) &&
                 _this.currentRotation.configure({
@@ -185,9 +137,9 @@ var TransformUi = /** @class */ (function (_super) {
                     angleX: value,
                 });
             _this.update();
+            changeInnerText("".concat(_this.options.idRotationValueX, "-value"), value.toString());
         };
-        angley.onchange = function () {
-            // this.currentRotationAxis = RotationAxis.Y;
+        angley.oninput = function () {
             var value = parseFloat(angley.value);
             !Number.isNaN(value) &&
                 _this.currentRotation.configure({
@@ -195,9 +147,9 @@ var TransformUi = /** @class */ (function (_super) {
                     angleY: value,
                 });
             _this.update();
+            changeInnerText("".concat(_this.options.idRotationValueY, "-value"), value.toString());
         };
-        anglez.onchange = function () {
-            // this.currentRotationAxis = RotationAxis.Z;
+        anglez.oninput = function () {
             var value = parseFloat(anglez.value);
             !Number.isNaN(value) &&
                 _this.currentRotation.configure({
@@ -205,30 +157,34 @@ var TransformUi = /** @class */ (function (_super) {
                     angleZ: value,
                 });
             _this.update();
+            changeInnerText("".concat(_this.options.idRotationValueZ, "-value"), value.toString());
         };
-        scaleX.onchange = function () {
+        scaleX.oninput = function () {
             var value = parseFloat(scaleX.value);
             !Number.isNaN(value) &&
                 _this.currentScale.configure({
                     sx: value,
                 });
             _this.update();
+            changeInnerText("".concat(_this.options.idScaleX, "-value"), value.toString());
         };
-        scaleY.onchange = function () {
+        scaleY.oninput = function () {
             var value = parseFloat(scaleY.value);
             !Number.isNaN(value) &&
                 _this.currentScale.configure({
                     sy: value,
                 });
             _this.update();
+            changeInnerText("".concat(_this.options.idScaleY, "-value"), value.toString());
         };
-        scaleZ.onchange = function () {
+        scaleZ.oninput = function () {
             var value = parseFloat(scaleZ.value);
             !Number.isNaN(value) &&
                 _this.currentScale.configure({
                     sz: value,
                 });
             _this.update();
+            changeInnerText("".concat(_this.options.idScaleZ, "-value"), value.toString());
         };
     };
     return TransformUi;
@@ -239,8 +195,6 @@ export var TRANSFORM_UI_DEFAULT_OPTIONS = {
     idTranslationX: "translation-x",
     idTranslationY: "translation-y",
     idTranslationZ: "translation-z",
-    //   idRotationType: "rotation-type",
-    //   idRotationValue: "rotation-value",
     idRotationValueX: "rotation-x",
     idRotationValueY: "rotation-y",
     idRotationValueZ: "rotation-z",
@@ -248,4 +202,8 @@ export var TRANSFORM_UI_DEFAULT_OPTIONS = {
     idScaleY: "scale-y",
     idScaleZ: "scale-z",
 };
+function changeInnerText(id, amount) {
+    var element = document.getElementById(id);
+    element.innerText = amount;
+}
 //# sourceMappingURL=TransformUi.js.map
